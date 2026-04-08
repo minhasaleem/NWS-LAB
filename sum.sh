07695039719c644477d3112c84ce5f1d60cf8746
minhasaleem <130979269+minhasaleem@users.noreply.github.com>
2026-04-08 16:08:59 +0530

    Add files via upload

---
+++ sum.sh
@@ -0,0 +1,19 @@
+echo "Enter a number"
+read num
+rev=0
+even=0
+odd=0
+while [ $num -gt 0 ]
+do
+tmp=$(( $num % 10 ))
+if(( $tmp % 2 == 0 ))
+then
+even=$(( $even + $tmp ))
+else
+odd=$(( $odd + $tmp ))
+fi
+rev=$(( $rev * 10 + $tmp ))
+num=$(( $num / 10 ))
+done
+echo "The sum of even numbers $even"
+echo "The sum of odd number $odd"
